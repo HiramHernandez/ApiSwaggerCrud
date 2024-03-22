52bd229ebf39a7e8cbf27115ac44714256920e92
hhg.mzt <hiram.hdezg@gmail.com>
2024-03-21 17:09:07 -0700

    chore: ejemplo de generación de token que servira de Guia para una más elaborado

--- src/server.py
+++ src/server.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
-from flask_marshmallow import Marshmallow
+from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from flask_jwt_extended import get_jwt_identity
 from .app.routes import api_blueprint
 from src.app.config.extensions import db, ma
 
@@ -10,6 +11,36 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_DATABASE')}"
+app.config['JWT_SECRET_KEY'] = 'super-secret' 
+
+jwt = JWTManager(app)
+
+@app.route('/login', methods=['POST'])
+def login():
+    # Obtener los datos del formulario de inicio de sesión
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+
+    # Verificar si las credenciales son correctas (datos fijos)
+    if username == 'admin' and password == '123':
+        # Crear el token de acceso
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"msg": "Credenciales inválidas"}), 401
+
+# Ruta protegida que requiere el token de autenticación
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    # Obtener la identidad del token
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
 db.init_app(app)
 ma.init_app(app)
 app.register_blueprint(api_blueprint)
+
+'''
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMTA2NTg3MCwianRpIjoiNjU3YTAzYzQtZTdiMy00MDJiLWE3ODgtMDZlM2JkNDBjZGY4IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImFkbWluIiwibmJmIjoxNzExMDY1ODcwLCJjc3JmIjoiYzc2M2JmYWEtYTQ4ZC00N2M1LWIwMTEtZWJhZjExZDJiNTg4IiwiZXhwIjoxNzExMDY2NzcwfQ.NolyusB-9r-6LuuoDATHeLKUeHBxzjrrlbmebBjrv7Q
+'''
